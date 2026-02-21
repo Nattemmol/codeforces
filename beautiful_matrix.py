6ba2dd18b65ce3e8f79dd397a0ab4e6437e58f61
Nattemmol <130242332+Nattemmol@users.noreply.github.com>
2026-02-21 17:44:22 +0300

    Add function to calculate distance in a matrix

---
+++ beautiful_matrix.py
@@ -0,0 +1,8 @@
+def beautiful_matrix():
+    matrix = [list(map(int, input().split())) for _ in range(5)]
+    for i in range(5):
+        for j in range(5):
+            if matrix[i][j] == 1:
+                print(abs(i - 2) + abs(j - 2))
+                return
+beautiful_matrix()
